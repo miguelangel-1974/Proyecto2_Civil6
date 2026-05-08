e9a3fc8b84a5226e1366b62c2baa8c556c4a4ee5
aitormg <aitormeragra@gmail.com>
2026-05-08 18:11:35 +0200

    Create ResourceGenerator.java

---
+++ src/M03/ResourceGenerator.java
@@ -0,0 +1,44 @@
+package M03;
+
+import java.util.TimerTask;
+
+public class ResourceGenerator extends TimerTask implements Variables {
+
+	private Civilization civilization;
+
+	public ResourceGenerator(Civilization civilization) {
+		this.civilization = civilization;
+	}
+
+	public Civilization getCivilization() {
+		return this.civilization;
+	}
+
+	public void setCivilization(Civilization civilization) {
+		this.civilization = civilization;
+	}
+
+	public void run() {
+		int newWood = this.civilization.getWood()
+				+ CIVILIZATION_WOOD_GENERATED
+				+ this.civilization.getCarpentry() * CIVILIZATION_WOOD_GENERATED_PER_CARPENTRY;
+
+		int newIron = this.civilization.getIron()
+				+ CIVILIZATION_IRON_GENERATED
+				+ this.civilization.getSmithy() * CIVILIZATION_IRON_GENERATED_PER_SMITHY;
+
+		int newFood = this.civilization.getFood()
+				+ CIVILIZATION_FOOD_GENERATED
+				+ this.civilization.getFarm() * CIVILIZATION_FOOD_GENERATED_PER_FARM;
+
+		int newMana = this.civilization.getMana()
+				+ this.civilization.getMagicTower() * CIVILIZATION_MANA_GENERATED_PER_MAGIC_TOWER;
+
+		this.civilization.setWood(newWood);
+		this.civilization.setIron(newIron);
+		this.civilization.setFood(newFood);
+		this.civilization.setMana(newMana);
+
+	}
+
+}
