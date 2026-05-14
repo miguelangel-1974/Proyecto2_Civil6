6a50dc17453d6595ccf0be9432f0a70392dea764
aitormg <aitormeragra@gmail.com>
2026-05-15 01:23:08 +0200

    .

--- src/M03/Civilization.java
+++ src/M03/Civilization.java
@@ -461,52 +461,41 @@ public class Civilization implements Variables{
 
 	}
 
-	public void newMagician(int n) throws ResourceException, BuildingException {
+	    public void newMagician(int n) throws ResourceException, BuildingException {
 
-	    if (this.magicTower < 1) {
-	        throw new BuildingException("No tienes Torres de mago para crear magos.");
-	    }
-	
-<<<<<<< HEAD
-	    int armor =0;
-	    int damage = BASE_DAMAGE_MAGICIAN + (this.technologyAtack * PLUS_ATTACK_MAGICIAN_BY_TECHNOLOGY * BASE_DAMAGE_MAGICIAN / 100);
-	
-	
-	    int creados = 0;
-	
-	    while (creados < n && this.iron >= IRON_COST_MAGICIAN
-=======
-	    int armor = 0;
-	    int damage = 0;
-	
-	    int creados = 0;
-	
-	    while (creados < n
-	            && this.iron >= IRON_COST_MAGICIAN
->>>>>>> 12f23dd648b781cbbedd5b0fa577a4a2dd3c5c0f
-	            && this.wood >= WOOD_COST_MAGICIAN
-	            && this.food >= FOOD_COST_MAGICIAN
-	            && this.mana >= MANA_COST_MAGICIAN) {
-	
-	        this.iron = this.iron - IRON_COST_MAGICIAN;
-	        this.wood = this.wood - WOOD_COST_MAGICIAN;
-	        this.food = this.food - FOOD_COST_MAGICIAN;
-	        this.mana = this.mana - MANA_COST_MAGICIAN;
-	
-	        this.army.get(7).add(new Magician(armor, damage));
-	
-	        creados+=1;
-	    }
-	
-	    if (creados > 0) {
-	        System.out.println("Se han reclutado " + creados + " magos.");
-	    }
-	
-	    if (creados < n) {
-	        throw new ResourceException("Recursos insuficientes. Querias " + n + " magos, solo se crearon " + creados + ".");
-	    }
-	}
-	
+        if (this.magicTower < 1) {
+            throw new BuildingException("No tienes Torres de mago para crear magos.");
+        }
+    
+        int armor = 0;
+        int damage = 0;
+    
+        int creados = 0;
+    
+        while (creados < n
+                && this.iron >= IRON_COST_MAGICIAN
+                && this.wood >= WOOD_COST_MAGICIAN
+                && this.food >= FOOD_COST_MAGICIAN
+                && this.mana >= MANA_COST_MAGICIAN) {
+    
+            this.iron = this.iron - IRON_COST_MAGICIAN;
+            this.wood = this.wood - WOOD_COST_MAGICIAN;
+            this.food = this.food - FOOD_COST_MAGICIAN;
+            this.mana = this.mana - MANA_COST_MAGICIAN;
+    
+            this.army.get(7).add(new Magician(armor, damage));
+    
+            creados+=1;
+        }
+    
+        if (creados > 0) {
+            System.out.println("Se han reclutado " + creados + " magos.");
+        }
+    
+        if (creados < n) {
+            throw new ResourceException("Recursos insuficientes. Querias " + n + " magos, solo se crearon " + creados + ".");
+        }
+    }
 	public void newPriest(int n) throws ResourceException, BuildingException {
 		if (church<1) {
 			throw new BuildingException("No tienes Iglesias para crear sacerdotes");
