792c4963e110fdd26f7ca0eebd84d533f8ab181e
Miguel Ángel Galdón Dios <galdondios@gmail.com>
2026-05-17 02:26:16 +0200

    Bug arreglado en Battle.java
    Se ha arreglado un bug que hacia que se escribiese el "CHANGE ATTACKER" mas veces de las que tendría que salir.

--- src/M03/Battle.java
+++ src/M03/Battle.java
@@ -272,9 +272,6 @@ public class Battle implements Variables {
 		while (this.remainderPercentageFleet(this.civilizationArmy) >= 20
 				&& this.remainderPercentageFleet(this.enemyArmy) >= 20) {
 
-			this.battleDevelopment = this.battleDevelopment
-					+ "********************CHANGE ATTACKER********************\n";
-
 			int grupoAtacante;
 			if (bandoAtacante == 0) {
 				grupoAtacante = this.getCivilizationGroupAttacker();
@@ -289,6 +286,9 @@ public class Battle implements Variables {
 				continue;
 			}
 
+			this.battleDevelopment = this.battleDevelopment
+					+ "********************CHANGE ATTACKER********************\n";
+			
 			int idxAtacante = (int) (Math.random() * grupoAt.size());
 			MilitaryUnit atacante = grupoAt.get(idxAtacante);
 
